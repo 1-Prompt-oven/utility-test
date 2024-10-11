81c28cd4607532cca8def845b7ab7fdfe2431072
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-10-11 19:15:44 +0900

    :tada: repository init by Spring Initializer

---
+++ src/main/java/com/example/utilitytest/UtilityTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.utilitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/utilitytest/UtilityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.utilitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
