cf09d99d194a1fc05f4c05f3a1c6a2c7dbe4f1d5
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-10-11 19:52:12 +0900

    :wrench: now use key from env

--- src/main/java/com/example/utilitytest/utility/Encrypter.java
+++ src/main/java/com/example/utilitytest/utility/Encrypter.java
@@ -1,34 +1,41 @@
 package com.example.utilitytest.utility;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
 @Component
 public class Encrypter {
 
-    private static final String Algo = "AES";
+    // 암호화 알고리즘 : AES-256 할 거에요
+    private static final String ALGO = "AES";
+    // 암호화 키
+    private final SecretKey secretKey;
 
-    public static String encrypt(String data, SecretKey key) throws Exception{
-        Cipher cipher = Cipher.getInstance(Algo);
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+    // 생성자에서 암호화 키를 설정파일로부터 초기화합니다.
+    public Encrypter(@Value("${encrypter.secret-key}") String encodedKey) {
+        byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+        this.secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, ALGO);
+    }
+
+    // 데이터를 암호화하는 메서드입니다.
+    public String encrypt(String data) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGO);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         byte[] encryptedData = cipher.doFinal(data.getBytes());
         return Base64.getEncoder().encodeToString(encryptedData);
     }
-    public static SecretKey generateKey() throws Exception{
-        KeyGenerator keyGen = KeyGenerator.getInstance(Algo);
-        keyGen.init(256); // AES-256 암호화 호출할 거에요
-        return keyGen.generateKey();
-    }
 
-    public static String decrypt(String encryptedData, SecretKey key) throws Exception{
-        Cipher cipher = Cipher.getInstance(Algo);
-        cipher.init(Cipher.DECRYPT_MODE, key);
+    // 암호화된 데이터를 복호화하는 메서드입니다.
+    public String decrypt(String encryptedData) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGO);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
         byte[] decoded = Base64.getDecoder().decode(encryptedData);
-        byte[] decrypteData = cipher.doFinal(decoded);
-        return new String(decrypteData);
+        byte[] decryptedData = cipher.doFinal(decoded);
+        return new String(decryptedData);
     }
 }
